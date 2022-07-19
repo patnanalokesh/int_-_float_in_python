cb3264706219bddc689bad1f29c4673a6a811731
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-19 19:54:40 +0530

    Add files via upload

---
+++ int&float.py
@@ -0,0 +1,31 @@
+# a=123
+# print(type(a))
+# b=12.250
+# print(type(b))
+# c=1.025
+# print(type(c))
+# print(type(124520120))
+# a=5 ;b=4
+# print(a)
+# print('%d'%b)
+
+# e=4.23
+# print(e)
+# print('%f'%e)
+# l=1.225
+# print('%.1f'%l)
+# print('%.2f'%l)
+# print('%.3f'%l)
+# print('%.4f'%l)
+
+''' Type Conversion & Casting '''
+# m=5634
+# print(type(m))
+# print(float(m))
+# print('%d'%m)
+# print('%f'%m)
+# n=5.23
+# print(type(n))
+# print(int(n))
+# # print('%d'%n)
+# print('%f'%n)
